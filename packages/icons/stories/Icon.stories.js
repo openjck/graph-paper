f19af0eebbe14678a5911720e6d000e0dabdc9e0
hamilton <hamilton.ulmer@gmail.com>
2020-04-10 13:44:31 -0700

    adds Photon & custon icons

---
+++ packages/icons/stories/Icon.stories.js
@@ -0,0 +1,14 @@
+import AllIconsStory from "./AllIcons.svelte";
+import { withKnobs, number, color } from "@storybook/addon-knobs";
+export default {
+  title: "Icon",
+  decorators: [withKnobs],
+};
+
+export const AllIcons = () => ({
+  Component: AllIconsStory,
+  props: {
+    size: number('size', 24),
+    color: color('color', '#000')
+  },
+});
